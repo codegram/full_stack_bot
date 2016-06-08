e5b9ee35ee1b62a490ade1e362aa7a75b3d36774
Josep Jaume Rey <josepjaume@gmail.com>
2016-06-08 19:43:34 +0200

    Fix specs

--- apps/bot_engine/lib/bot_engine/api_ai.ex
+++ apps/bot_engine/lib/bot_engine/api_ai.ex
@@ -15,7 +15,6 @@ defmodule BotEngine.ApiAi do
 
     response = HTTPoison.get!(resource_url("query"), headers, params: params)
     |> process_body
-    |> IO.inspect
     |> generate_response
 
     {:ok, response}
